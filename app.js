702362ddfddaf6322fad0b992a1ccb46db3a461f
zinklog <guptaishan718@gmail.com>
2020-06-16 03:48:53 +0530

    add capability of cloud based database server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ishan:karela@cluster0-fv3f5.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
