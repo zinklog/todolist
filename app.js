16870278013e8bc9e6e8470cda4e3ee23f8c0143
zinklog <guptaishan718@gmail.com>
2020-06-16 04:51:53 +0530

    updates Procfile

--- app.js
+++ app.js
@@ -136,11 +136,6 @@ app.get("/about", function(req, res){
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
 app.listen(port, function() {
   console.log("Server has started");
 });
